aff50f6f7c028a1c0d43e8b933d6b5c76497e604
vibhanshu verma <105881233+vibhanshuverma02@users.noreply.github.com>
2024-02-01 23:53:36 +0530

    database using no sql mongoDB atlas

---
+++ database /index.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect("mongodb+srv://vibhhh:vibh1223@cluster1.czdbjcw.mongodb.net/course_selling_app");
+
+// Define schemas
+const AdminSchema = new mongoose.Schema({
+    // Schema definition here
+    username: String,
+    password: String
+});
+
+const UserSchema = new mongoose.Schema({
+    // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
+});
+
+const CourseSchema = new mongoose.Schema({
+    // Schema definition here
+    title: String,
+    description: String,
+    imageLink: String,
+    price: Number
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
